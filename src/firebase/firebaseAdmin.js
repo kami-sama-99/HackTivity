78f8125603f45109846a473d2a8b6d133a0b6477
kami_sama_99 <parthshende12@gmail.com>
2025-03-03 22:35:58 +0530

    Deployment test 2

--- src/firebase/firebaseAdmin.js
+++ src/firebase/firebaseAdmin.js
@@ -4,7 +4,7 @@ import { getApps } from "firebase-admin/app";
 if (!getApps().length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Fix newlines
     }),
